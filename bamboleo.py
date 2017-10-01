4f448e41fdf97cfa9f69d62d039b2cade0f081b5
Pablo Odorico <pablo.odorico@gmail.com>
2017-10-01 15:12:54 +0800

    Segmentation using graph-cuts

--- bamboleo.py
+++ bamboleo.py
@@ -6,11 +6,13 @@
 import os
 import sys
 import argparse
+import operator
 
 # 3rd party modules
 try:
     import cv2
     import numpy as np
+    import matplotlib
     from matplotlib import pyplot as plt
 except ImportError as e:
     print(str(e) + '\nPlease install required packages with:\n  pip3 install opencv-python matplotlib numpy')
@@ -18,7 +20,6 @@ except ImportError as e:
 
 # Global settings
 args = None
-enablePlots = True
 
 #
 # Utils
@@ -30,36 +31,63 @@ def px2cm(pixels): return pixels / dpi2dpcm(args.dpi)
 def cm2px(cm): return cm * dpi2dpcm(args.dpi)
 
 figureCount = 0
-class Figure():
+class Figure:
     ''' Scope-based plotting util that will name and save figures in the output directory. '''
-    def __init__(self, title=''):
+    def __init__(self, title='', filetype='png'):
         self.title = title
+        self.filetype = filetype
     def __enter__(self):
         plt.figure()
         plt.axis('off')
-        if self.title: plt.title(self.title, loc='left')
+        if self.title:
+            plt.title(self.title, loc='left')
+            self.title = self.title.replace('\n', ' ')
     def __exit__(self, type, value, traceback):
         global figureCount
-        path = os.path.join(args.output, f'figure_{figureCount}.png')
+        path = os.path.join(args.output, f'figure_{figureCount}.{self.filetype}')
         print(f'> Saving figure {figureCount}: {self.title}')
         plt.savefig(path, bbox_inches='tight', dpi=250)
         plt.close()
         figureCount += 1
 
 class Contour:
-    def __init__(self, contour, treeNode):
-        self.contour = contour
+    ''' Wraps a contour obtained with findContours '''
+    def __init__(self, points, treeNode=None):
+        self.points = points
         self.treeNode = treeNode
-        self._area = None
-        self._rect = None
+        # Place-holder properties. Functions will be called the first time the property function is called.
+        self._area = cv2.contourArea
+        self._rect = cv2.boundingRect
+        self._ellipse = cv2.fitEllipse
+        self._moments = cv2.moments
+        self._perimeter = cv2.arcLength
 
     def area(self):
-        if self._area is None: self._area = cv2.contourArea(self.contour)
+        if callable(self._area): self._area = self._area(self.points)
         return self._area
     def rect(self):
-        ''' Returns (x, y, w, h) '''
-        if self._rect is None: self._rect = cv2.boundingRect(self.contour)
+        ''' (x, y, width, height) '''
+        if callable(self._rect): self._rect = self._rect(self.points)
         return self._rect
+    def ellipse(self):
+        if len(self.points) < 5: return None
+        if callable(self._ellipse): self._ellipse = self._ellipse(self.points)
+        return self._ellipse
+    def moments(self):
+        if callable(self._moments): self._moments = self._moments(self.points)
+        return self._moments
+    def perimeter(self):
+        if callable(self._perimeter): self._perimeter = self._perimeter(self.points, True)
+        return self._perimeter
+
+    def centroid(self):
+        m = self.moments()
+        return (m['m10'] / m['m00'], m['m01'] / m['m00'])
+    def aspectRatio(self):
+        rect = self.rect()
+        return rect[2] / rect[3] # width / height
+    def equivalentDiamater(self):
+        return np.sqrt(4 * self.area() / np.pi)
 
     def __repr__(self):
         return f'[rect: {self.rect()}, area: {self.area()} px]'
@@ -68,8 +96,23 @@ def Contours(contourArray, treeArray):
     ''' Create a list of Contour from the output of cv2.findContour '''
     return [Contour(contourArray[i], treeArray[0, i, :]) for i in range(len(contourArray))]
 
+def contourMayBeTube(contour):
+    '''
+    Basic tube-ness test. Returns false when contour is for sure not a valid tube.
+    TODO: Maybe check how well it fits an ellipse
+    '''
+    minRectSide = cm2px(2.0)
+    return contour.rect()[2] > minRectSide and contour.rect()[3] > minRectSide
+
+def contourMayBeHole(contour):
+    '''
+    Basic hole-ness test. Returns false when contour is for sure not a valid tube hole.
+    Use only on contours inside a contourMayBeTube() contour.
+    '''
+    minRectSide = cm2px(1.0)
+    return contour.rect()[2] > minRectSide and contour.rect()[3] > minRectSide
 #
-# First stage: Image segmentation and finding cut contours (two approaches)
+# First stage: Image segmentation to find tube contours (two approaches)
 #
 
 def findTopLevelContours1(grayscale):
@@ -80,7 +123,7 @@ def findTopLevelContours1(grayscale):
     # TODO: If a fixed seed causes problem iterate with random seed positions near the corners
     mask = np.zeros((height + 2, width + 2), np.uint8)
     cv2.floodFill(grayscale, mask, (5, 5), 255, 1, 12, flags=4 | cv2.FLOODFILL_MASK_ONLY)
-    # Crop mask to image size, invert to have 1 on the cuts and 0 on the background
+    # Crop mask to image size, invert to have 1 on the tubes and 0 on the background
     mask = mask[1:height+1, 1:width+1]
     mask = 1 - mask
 
@@ -92,11 +135,16 @@ def findTopLevelContours1(grayscale):
     _, contourArray, treeArray = cv2.findContours(filteredMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = Contours(contourArray, treeArray)
 
-    if enablePlots:
+    if args.plots:
         with Figure('Background flood-fill mask'):
             plt.imshow(mask * 255, cmap='gray')
-        with Figure('Filtered background mask'):
-            plt.imshow(filteredMask * 255, cmap='gray')
+        with Figure('Filtered mask and detected tube rectangles'):
+            render = np.zeros((height, width, 3), np.uint8)
+            render[filteredMask == 1] = (255, 255, 255)
+            for c in contours:
+                x, y, w, h = c.rect()
+                cv2.rectangle(render, (x, y), (x+w, y+h), (0, 255 ,0), 2)
+            plt.imshow(render)
 
     return contours
 
@@ -109,38 +157,138 @@ def findTopLevelContours2(grayscale):
     # Apply morphological closing (dilatation followed by erosion) to close some of the internal holes and
     # reduce the number of contours
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    closed = cv2.morphologyEx(binarized, cv2.MORPH_CLOSE, kernel, iterations=2)
+    filteredMask = cv2.morphologyEx(binarized, cv2.MORPH_CLOSE, kernel, iterations=2)
 
     # Extract top-level contours and organize them in a list of Contour objects.
-    _, contourArray, treeArray = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    _, contourArray, treeArray = cv2.findContours(filteredMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = Contours(contourArray, treeArray)
 
-    if enablePlots:
+    if args.plots:
         with Figure('Luminance histogram'):
             plt.axis('on'), plt.xticks([]), plt.yticks([])
             plt.hist(grayscale.ravel(), 128)
             plt.axvline(x=threshold, color='r', linestyle='dashed', linewidth=2)
         with Figure(f'Binarized image (threshold = {int(threshold)})'):
             plt.imshow(binarized, cmap='gray')
-        with Figure(f'Filtered mask'):
-            plt.imshow(closed, cmap='gray')
+        with Figure(f'Filtered mask with possible contour rects'):
+            height, width = grayscale.shape[:2]
+            render = np.zeros((height, width, 3), np.uint8)
+            render[filteredMask != 0] = (255, 255, 255)
+            for c in contours:
+                x, y, w, h = c.rect()
+                cv2.rectangle(render, (x, y), (x+w, y+h), (0, 255 ,0), 2)
+            plt.imshow(render)
 
     return contours
 
+def extractTube(inputImage, contour, tubeId):
+
+    def scaledPoints(contour, k):
+        ''' Returns points of Contour scaled around its center. '''
+        center = np.array(contour.centroid())
+        points = k * (contour.points - center) + center
+        return points.astype(np.int32, copy=False)
+
+    def scaledRect(rect, k, width, height):
+        ''' Scale rect maintaining center and clap to (0,0) to (width-1, height-1). '''
+        x, y, w, h = rect
+        pos, size = np.array((x, y)), np.array((w, h))
+        center = pos + size / 2
+        pos, size = k * (pos - center) + center, k * size
+        pos = np.clip(pos, [0, 0], [width-1, height-1]).astype(int)
+        size = np.clip(pos + size, [0, 0], [width-1, height-1]).astype(int) - pos
+        return (pos[0], pos[1], size[0], size[1])
+
+    print(f'* Processing tube {tubeId}...')
+
+    # Scale the contour by 1.1x and to get a bounding rect that contains every tube pixel.
+    inputHeight, inputWidth = inputImage.shape[:2]
+    x, y, w, h = scaledRect(contour.rect(), 1.1, inputWidth, inputHeight)
+    # Crop the expanded rect tube
+    tubeImage = inputImage[y:y+h, x:x+w]
+
+    # Create and empty "sure background" mask then draw concentric contours with different labels
+    # Mask is full input size to avoid problems scaling the cropped contour
+    mask = np.full((inputHeight, inputWidth), cv2.GC_BGD, np.uint8)
+    hints = [
+        (1.05, cv2.GC_PR_BGD),
+        (0.90, cv2.GC_FGD),
+        (0.80, cv2.GC_PR_BGD),
+        (0.25, cv2.GC_BGD)
+    ]
+    for scale, label in hints:
+        cv2.drawContours(mask, [scaledPoints(contour, scale)], -1, label, -1)
+    # Crop mask
+    mask = mask[y:y+h, x:x+w]
+
+    # Run graph-cuts
+    bgdModel = np.zeros((1, 65), np.float64)
+    fgdModel = np.zeros((1, 65), np.float64)
+    outputMask = mask.copy()
+    cv2.grabCut(tubeImage, outputMask, None, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_MASK)
+
+    # Convert graph-cuts label output to a binary mask (1 for foreground)
+    outputMask = np.where((outputMask == cv2.GC_BGD) | (outputMask == cv2.GC_PR_BGD), 0, 1).astype('uint8')
+
+    # Morphologic opening to discard small islands
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    outputMask = cv2.morphologyEx(outputMask, cv2.MORPH_OPEN, kernel, iterations=1)
+
+    # Extract full contour hierarchy from the output mask
+    _, contourArray, treeArray = cv2.findContours(outputMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    tubeContours = Contours(contourArray, treeArray)
+
+    # Find the largest top-level contour. This is the outside tube contour.
+    topLevelContours = [c for c in tubeContours if c.treeNode[3] == -1]
+    outsideContour = max(topLevelContours, key=operator.methodcaller('area'))
+
+    # Find the largest contour which is contained in the top-level contour. This is the inside tube contour.
+    secondLevelContours = [c for c in tubeContours if c.treeNode[3] == topLevelContours.index(outsideContour)]
+    insideContour = max(secondLevelContours, key=operator.methodcaller('area'))
+
+    print(f'* Found {len(topLevelContours)} top-level contours, {len(secondLevelContours)} sub-contours.')
+
+    if args.plots:
+        with Figure(f'Tube {tubeId}: Graph-cuts input (red: sure background, green: sure foreground)'):
+            renderMask = np.zeros((h, w, 3), np.uint8)
+            renderMask[mask == cv2.GC_FGD] = (0, 200, 0)
+            renderMask[mask == cv2.GC_BGD] = (255, 0, 0)
+            render = cv2.addWeighted(tubeImage, 0.7, renderMask, 0.3, 0)
+            plt.imshow(render)
+        with Figure(f'Tube {tubeId}: Graph-cuts output'):
+            renderMask = np.zeros((h, w, 3), np.uint8)
+            renderMask[outputMask == 0] = (255, 0, 0)
+            render = cv2.addWeighted(tubeImage, 0.7, renderMask, 0.3, 0)
+            plt.imshow(render)
+
+        odCm = px2cm(outsideContour.equivalentDiamater())
+        idCm = px2cm(insideContour.equivalentDiamater())
+        opCm = px2cm(outsideContour.perimeter())
+        ipCm = px2cm(insideContour.perimeter())
+        with Figure(f'Tube {tubeId}: Final contours\n OD = {odCm:.1f} cm, OP = {opCm:.1f} cm\n ID = {idCm:.1f} cm, IP = {ipCm:.1f} cm'):
+            render = tubeImage.copy()
+            cv2.drawContours(render, [outsideContour.points], -1, (255, 0, 255), 3)
+            cv2.drawContours(render, [insideContour.points], -1, (255, 255, 0), 3)
+            plt.imshow(render)
+
+    return (outsideContour, insideContour, tubeImage)
+
+
 def main():
     # Load image
-    print(f'Loading "{args.inputPath}".')
     inputImageBGR = cv2.imread(args.inputPath)
     if inputImageBGR is None:
         print(f'Failed to open image file "{args.inputPath}".')
         return
+    inputHeight, inputWidth = inputImageBGR.shape[:2]
+    print(f'Loaded "{args.inputPath}": {inputWidth}x{inputHeight} px at {args.dpi} DPI is {px2cm(inputWidth):.1f}x{px2cm(inputHeight):.1f} cm.')
 
     # Convert BGR to RGB
     inputImage = cv2.cvtColor(inputImageBGR, cv2.COLOR_BGR2RGB)
     # Convert to grayscale
     grayscale = cv2.cvtColor(inputImageBGR, cv2.COLOR_RGB2GRAY)
 
-    if enablePlots:
+    if args.plots:
         with Figure('Input image'):
             plt.imshow(inputImage)
 
@@ -148,43 +296,39 @@ def main():
             plt.imshow(grayscale, cmap='jet')
             plt.colorbar(orientation='horizontal')
 
-    # Get candidate cut contours
+    # Get candidate tube contours
     topLevelContours = findTopLevelContours1(grayscale)
 
-    # Discard contours that are too small: both sides of the bounding rectangle have to be above a minRectSide
-    minRectSide = cm2px(2.0)
-    cutsContours = [c for c in topLevelContours if c.rect()[2] > minRectSide and c.rect()[3] > minRectSide]
+    # Discard contours that fail a basic tube-ness test.
+    tubesContours = [c for c in topLevelContours if contourMayBeTube(c)]
 
-    if not cutsContours:
-        print('No cuts found!')
+    if not tubesContours:
+        print('No tubes found!')
         return
 
-    print(f'Found {len(cutsContours)} cuts:')
-    for i, cut in enumerate(cutsContours):
-        print(f'  Cut {i+1}: Area {cut.area()} px, bounding rectangle size {cut.rect()[2:]} px')
-    if len(cutsContours) != len(topLevelContours):
-        print(f'Discarded {len(topLevelContours)-len(cutsContours)} contours deemed too small.')
+    print(f'Found {len(tubesContours)} tubes:')
+    for i, tube in enumerate(tubesContours):
+        height, width = tube.rect()[2:]
+        print(f'  Tube {i+1}: Bounding rectangle size {px2cm(width)*10:.0f}x{px2cm(height)*10:.0f} mm.')
+    if len(tubesContours) != len(topLevelContours):
+        print(f'Discarded {len(topLevelContours)-len(tubesContours)} contours deemed too small.')
 
-    if enablePlots:
-        with Figure('Detected cuts'):
-            render = inputImage.copy()
-            for c in topLevelContours:
-                if len(c.contour) < 5: continue
-                color = (255, 0, 255) if c in cutsContours else (0, 0, 255)
-                ellipse = cv2.fitEllipse(c.contour)
-                cv2.ellipse(render, ellipse, color, 2)
-            for c in cutsContours:
-                x, y, w, h = c.rect()
-                cv2.rectangle(render, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    for tubeId, contour in enumerate(tubesContours):
+        outsideContour, insideContour, tubeRectImage = extractTube(inputImage, contour, tubeId + 1)
+
+        with Figure(f'Tube {tubeId}'):
+            render = np.zeros_like(tubeRectImage)
+            render[:, :, :] = (255, 255, 255)
+            cv2.drawContours(render, [outsideContour.points, insideContour.points], -1, (0, 0, 0), 3)
             plt.imshow(render)
-
 
 if __name__ == '__main__':
     # Parse command-line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('inputPath', help='image to process', metavar='image.jpg')
     parser.add_argument('dpi', help='image dots-per-inch', metavar='dpi', type=int)
-    parser.add_argument('--output', help='output directory', metavar='output/', default='output')
+    parser.add_argument('--output', help='output directory', metavar=f'output{os.path.sep}', default='output')
+    parser.add_argument('--plots', help='generate intermediate plots', action='store_true')
     args = parser.parse_args()
 
     # Get absolute path out output dir, create if it doesn't exist
@@ -193,34 +337,3 @@ if __name__ == '__main__':
         print(f'Created output directory "{args.output}".')
 
     main()
-
-
-'''
-Watershed sample code
-
-    opening = cv2.morphologyEx(binarized, cv2.MORPH_OPEN,kernel, iterations=2)
-# sure background area
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-sure_bg = cv2.dilate(closed, kernel, iterations=3)
-# Finding sure foreground area
-dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
-ret, sure_fg = cv2.threshold(dist_transform, 0.2 * dist_transform.max(), 255, 0)
-# Finding unknown region
-sure_fg = np.uint8(sure_fg)
-unknown = cv2.subtract(sure_bg, sure_fg)
-# Marker labelling
-ret, markers = cv2.connectedComponents(sure_fg)
-# Add one to all labels so that sure background is not 0, but 1
-markers = markers+1
-# Now, mark the region of unknown with zero
-markers[unknown==255] = 0
-markers = cv2.watershed(inputImage, markers)
-inputImage[markers == -1] = [255,0,0]
-#cv2.imshow('1opening',opening)
-cv2.imshow('2sure_bg',sure_bg)
-#cv2.imshow('3dist_transform', dist_transform / dist_transform.max() * 255)
-cv2.imshow('4sure_fg',sure_fg)
-#cv2.imshow('4unknown',unknown)
-cv2.imshow('markers',inputImage)
-cv2.waitKey()
-'''
